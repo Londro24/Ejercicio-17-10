ee42620435ea8bc0744691e86e3a5dc9fe97b5a2
Londro24 <alejandro.ide.figueroa@gmail.com>
2022-10-20 20:00:24 -0300

    a

---
+++ ejercicio/src/main.rs
@@ -0,0 +1,56 @@
+use std::fs::File;
+use std::path::Path;
+use std::io::Read;
+use std::fs::OpenOptions;
+use std::io::Write;
+
+fn read_file(mut f: &File){
+    let mut text = String::new();
+    f.read_to_string(&mut text).unwrap();
+    println!("{}", &text);
+}
+
+fn create_blank_file(p: &Path){
+    let _file = File::create(p).expect("El archivo no pudo crearse");
+    println!("El archivo fue creado");
+}
+
+
+fn add_new_content(mut f: &File){
+    f.write_all(b"Nuevo texto\n");
+}
+
+fn open_file_to_append(p: &Path) -> File{
+
+    let mut binding = OpenOptions::new();
+    let binding = binding.append(true);
+    let file = match binding.open(p){
+        Err(_why) => panic!("No se puede abrir el archivo"),
+        Ok(file) => file,
+    };
+
+    //add_new_content(&file);
+
+    return file
+}
+
+fn open_file_to_read(p: &Path){
+    if Path::new(p).exists(){
+        let file = match File::open(&p){
+            Err(_why) => panic!("El archivo no se puede abrir..."),
+            Ok(file) => file,
+        };
+        read_file(&file)
+    } else {
+        create_blank_file(p);
+    }
+}
+
+
+fn main(){
+    let path = Path::new("./hola.txt");
+    open_file_to_read(path);
+    let file = open_file_to_append(path);
+    add_new_content(&file);
+    open_file_to_read(path);
+}
---
+++ ejercicio/target/debug/ejercicio.d
@@ -0,0 +1,1 @@
+C:\Users\aleja\Documents\GitHub\Ejercicio-17-10\ejercicio\target\debug\ejercicio.exe: C:\Users\aleja\Documents\GitHub\Ejercicio-17-10\ejercicio\src\main.rs
